33ac9dda095ec72c7fb4deadf93f457a79170e08
sohan-99 <sohan75632@gmail.com>
2024-05-15 14:24:10 +0600

    Initial setup done Frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Navber from './components/Navber'
+
+function App() {
+  return (
+    <>
+      <Navber />
+    </>
+  )
+}
+
+export default App
