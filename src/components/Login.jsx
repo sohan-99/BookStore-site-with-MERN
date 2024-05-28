de66cf7b8c1662dfdac4afc37044455302d9f48d
sohan-99 <sohan75632@gmail.com>
2024-05-28 16:55:13 +0600

    tiny work

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Login() {
+    return (
+        <div>
+            Login
+
+        </div>
+    )
+}
+
+export default Login
---
+++ src/components/Signup.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Signup() {
+    return (
+        <div>
+            Signup
+        </div>
+    )
+}
+
+export default Signup
